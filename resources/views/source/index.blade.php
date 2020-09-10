8510da00abd4612fba8c35e34555e6f73a04e4b0
ynishihiro <Yumi@users.noreply.github.com>
2020-09-10 11:25:01 -0300

    Troca a rota para uma única, e mostra se o centro tem apenas despesa ou não

--- resources/views/source/index.blade.php
+++ resources/views/source/index.blade.php
@@ -8,18 +8,24 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h1> Centros de Despesa </h1>
+            <h1> Centros do CEPETI </h1>
             <br>
 
-            <x-calendar value="{{ $month->name }} {{ $year }}"></x-calendar>
+            <!--<x-calendar value="{{ $month->name }} {{ $year }}"></x-calendar> --> 
 
             @foreach($sources as $source)
                 <tr>
                     <th>
                         <br>
+                        @if($source->income == "0")
                         <h4 style="text-align: center">
-                            <a class="botao" href="{{ URL::to('despesa/' . $source->slug) }}">{{$source->name}}</a>
+                            <b>{{$source->name}}</b> <br><a class="botao" href="{{ URL::to('centros/' . $source->slug) }}">Despesa e Receita</a>
                         </h4>
+                        @else
+                        <h4 style="text-align: center">
+                            <b>{{$source->name}}</b> <br><a class="botao" href="{{ URL::to('despesa/' . $source->slug) }}">Despesa</a>
+                        </h4>
+                        @endif
                         <br>
                     </th>
                 </tr>
