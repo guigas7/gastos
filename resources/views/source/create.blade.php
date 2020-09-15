e89664cdb74aa7acc83370d9039a65c1880cf07d
ynishihiro <Yumi@users.noreply.github.com>
2020-09-15 11:43:42 -0300

    Adiciona script para adicionar despesas

--- resources/views/source/create.blade.php
+++ resources/views/source/create.blade.php
@@ -1,88 +1,59 @@
 @extends('layouts.login')
+<script src="{{ asset('js/despesas.js') }}"></script>
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center center-vertical">
-        <div class="card-body">
-            <br>
-            <div class="card">
-                <div class="card-header">{{ __('Criar novo Centro') }}</div>
-                    <div class="card-body">
-                        <div class="cent">
-                            <form method="POST" action="/centros/criar">
-                                @csrf
+    <div class="row justify-content-center">
+        <br>
+        <h2>{{ __('Criar novo Centro') }}</h2>
+        <br>
+    </div>
+    <br><br>
+    <form method="POST" action="/centros/criar">
+    @csrf
+    <div class="form-group row">
+        <label for="name" class="col-md-5 col-form-label text-md-center">{{ __('Nome') }}</label>
+        <div class="col-md-5">
+            <input id="name" type="string" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autofocus>
 
-                                <div class="form-group row">
-                                    <label for="name" class="col-md-5 col-form-label text-md-center">{{ __('Nome') }}</label>
+            @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        </div>
+        <hr>
 
-                                    <div class="col-md-5">
-                                        <input id="name" type="string" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autofocus>
+        <div class="form-group row mb-0">
+            <label for="income" class="col-md-5 col-form-label text-md-center">{{ __('Este centro também é um centro de Receita?') }}</label>
 
-                                        @error('name')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                </div>
-                                <hr>
-
-                                <div class="form-group row">
-                                    <label for="slug" class="col-md-5 col-form-label text-md-center">{{ __('Link para acesso') }}</label>
-
-                                    <div class="col-md-5">
-                                        <input id="slug" type="string" class="form-control @error('slug') is-invalid @enderror" name="name" value="{{ old('description') }}" required autofocus>
-
-                                        @error('name')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                </div>
-                                <hr>
-
-                                <div class="form-group row">
-                                    <label for="description" class="col-md-5 col-form-label text-md-center">{{ __('Descrição') }}</label>
-
-                                    <div class="col-md-5">
-                                        <input id="description" type="string" class="form-control @error('description') is-invalid @enderror" name="description" value="{{ old('description') }}" required autofocus>
-
-                                        @error('name')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                </div>
-                                <hr>
-
-                                <div class="form-group row mb-0">
-                                    <label for="income" class="col-md-5 col-form-label text-md-center">{{ __('Este centro também é um centro de Receita?') }}</label>
-
-                                    <div class="col-md-2">
-                                        <p>
-                                            <input type="radio" id="vent-on" name="income" required value="1">
-                                            <label for="income">Sim</label>
-                                            <input type="radio" id="vent-off" name="income" value="0">
-                                            <label for="income">Não</label>
-                                        </p>
-                                    </div>
-                                </div>
-                                <br>
-
-                                <div class="form-group row mb-0">
-                                    <button id="enviar" type="submit" class="btn btn-primary bt">
-                                        {{ __('Enviar') }}
-                                    </button>
-                                </div>
-
-                            </form>
-                        </div>
-                    </div>
-                </div>
+            <div class="col-md-2">
+                <p>
+                    <input type="radio" id="vent-on" name="income" required value="1">
+                        <label for="income">Sim</label>
+                        <br>
+                    <input type="radio" id="vent-off" name="income" value="0">
+                        <label for="income">Não</label>
+                </p>
             </div>
         </div>
-    </div>
+        <br>
+
+        <div class="form_container">
+            <button class="add_form_fields">Inserir novo tipo de despesa
+                <span style="font-size:16px; font-weight:bold;">+ </span>
+            </button>
+        </div>
+        <br><br>
+
+        <div class="form-group row mb-0">
+            <button id="enviar" type="submit" class="btn btn-primary bt">
+                {{ __('Enviar') }}
+            </button>
+        </div>
+
+    </form>
+
 </div>
 @endsection
